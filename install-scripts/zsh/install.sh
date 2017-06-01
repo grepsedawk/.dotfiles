4ef8b40ae7d76d31e7b5d3bca38ddc5a6fc90814
Alex Piechowski <alex@piechowski.org>
2017-06-01 03:32:09 -0500

    Add travis change to not require a password to chsh

--- install-scripts/zsh/install.sh
+++ install-scripts/zsh/install.sh
@@ -3,5 +3,5 @@ sudo apt install -y zsh
 if [ ! -d ~/.oh-my-zsh ]; then
   git clone git@github.com:robbyrussell/oh-my-zsh.git ~/.oh-my-zsh || git clone https://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
-chsh -s /bin/zsh
+sudo chsh -s /bin/zsh
 
