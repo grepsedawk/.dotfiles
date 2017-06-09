bc941781da3b7d35449874535ff87c8cacbc0e0d
Alex Piechowski <alex@piechowski.org>
2017-06-09 05:16:13 -0500

    Fix issue with jetbrains toolbox install

--- install-scripts/jetbrains-toolbox/install.sh
+++ install-scripts/jetbrains-toolbox/install.sh
@@ -2,5 +2,6 @@ version=1.3.2421
 cd /tmp
 wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-${version}.tar.gz
 tar zxf jetbrains-toolbox-${version}.tar.gz
+mkdir -p $HOME/.bin/
 mv jetbrains-toolbox-${version}/jetbrains-toolbox ~/.bin/
 
