0356890b76568589ffbc56748b81c621b76668b5
Alex Piechowski <alex@piechowski.org>
2017-06-09 05:28:11 -0500

    Install 2 stable Rubies and NodeJS LTS upon asdf install

--- install-scripts/asdf/install.sh
+++ install-scripts/asdf/install.sh
@@ -1,3 +1,15 @@
 #! /usr/bin/env bash
-git clone git@github.com:asdf-vm/asdf.git ~/.asdf --branch v0.3.0 || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+if [ ! -d ~/.asdf ]; then
+  git clone git@github.com:asdf-vm/asdf.git ~/.asdf --branch v0.3.0 || git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.3.0
+fi
+
+. $HOME/.asdf/asdf.sh
+
+asdf plugin-add ruby https://github.com/asdf-vm/asdf-ruby.git
+asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+
+asdf install ruby 2.4.1
+asdf install ruby 2.3.4
+
+asdf install nodejs 6.11.0
 
