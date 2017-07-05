8d2be8cea23de0b9ed50196e0184cd51ddb5dedb
Alex Piechowski <alex@piechowski.org>
2017-07-05 14:06:35 -0500

    Install xclip, xautolock
    Resolves #9

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install -y vim i3 git tmux shutter rxvt-unicode feh \
 	silversearcher-ag build-essential tcl libqt4-dev libqtwebkit-dev \
-        scrot redshift
+        scrot redshift xautolock xclip
 
 if [ ! -d ~/.dotfiles ]; then
   git clone git@github.com:pachonk/.dotfiles.git ~/.dotfiles || git clone https://github.com/pachonk/.dotfiles ~/.dotfiles
