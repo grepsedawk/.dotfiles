7d5cc8ca00dd861a7a377fece6e23983d1955ff7
Alex Piechowski <alex@piechowski.io>
2017-07-11 12:14:24 -0500

    Don't install jetbrains-toolbox this time

--- install-scripts/install.sh
+++ install-scripts/install.sh
@@ -17,7 +17,7 @@ $HOME/.dotfiles/install-scripts/redis/install-redis.sh
 $HOME/.dotfiles/install-scripts/thefuck/install.sh
 $HOME/.dotfiles/install-scripts/asdf/install.sh
 $HOME/.dotfiles/install-scripts/bc3/install.sh
-$HOME/.dotfiles/install-scripts/jetbrains-toolbox/install.sh
+# $HOME/.dotfiles/install-scripts/jetbrains-toolbox/install.sh
 $HOME/.dotfiles/install-scripts/postgresql/install.sh
 $HOME/.dotfiles/install-scripts/maria-db/install.sh
 
