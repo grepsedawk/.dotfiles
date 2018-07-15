25af11f04b28041851b94b940babd5267ec3bbb6
Alex Piechowski <alex@piechowski.io>
2018-07-15 00:45:00 -0500

    Remove jetbrains toolbox install script entirely

--- install-scripts/jetbrains-toolbox/install.sh
+++
@@ -1,7 +0,0 @@
-version=1.3.2421
-cd /tmp
-wget https://download.jetbrains.com/toolbox/jetbrains-toolbox-${version}.tar.gz
-tar zxf jetbrains-toolbox-${version}.tar.gz
-mkdir -p $HOME/.bin/
-mv jetbrains-toolbox-${version}/jetbrains-toolbox ~/.bin/
-
