8f33b65f085aee1046c210cf942a96f2f22e8fa1
Alex Piechowski <alex@piechowski.io>
2018-09-13 13:34:59 -0500

    Switch to nvim

--- .zshrc
+++ .zshrc
@@ -31,3 +31,7 @@ eval $(thefuck --alias qwer)
 stty -ixon
 
 [ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+if type nvim > /dev/null 2>&1; then
+  alias vim='nvim'
+fi
--- install-scripts/vim/install.sh
+++ install-scripts/vim/install.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/env bash
-sudo apt install -y vim
+sudo apt install -y neovim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# bootstrap nvim to use vim config locations
+mkdir -p ~/.config/nvim/
+cat << EOF > $HOME/.config/nvim/init.vim
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
+EOF
