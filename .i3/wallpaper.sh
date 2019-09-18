1cd1a28e729e7cb33d62a0505d4428b3392d6940
Alex Piechowski <alex@piechowski.io>
2019-09-18 11:46:31 -0600

    Fix i3 wallpaper

--- .i3/wallpaper.sh
+++ .i3/wallpaper.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-WALLPAPERS=/home/alex/.i3/wallpaper/
+WALLPAPERS="$HOME"/.i3/wallpaper/
 TIME=1m
 while [ true ];
 do
-    feh --randomize --bg-fill $WALLPAPERS && sleep $TIME
+    feh --randomize --bg-fill "$WALLPAPERS" && sleep $TIME
 done &
