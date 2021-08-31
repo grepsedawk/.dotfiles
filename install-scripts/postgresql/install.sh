2e63ae24d78df962935094279eb4aa4806bfb175
Alex Piechowski <alex@piechowski.io>
2021-08-30 22:47:20 -0600

    Install pg 13 instead

--- install-scripts/postgresql/install.sh
+++ install-scripts/postgresql/install.sh
@@ -1,4 +1,7 @@
 #! /usr/bin/env bash
-sudo apt install -y postgresql libpq-dev
+curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+sudo apt-get update
+sudo apt-get install -y --no-install-recommends postgresql-13 libpq-dev
 sudo service postgresql start
 sudo -u postgres psql -c "CREATE ROLE `whoami` WITH SUPERUSER LOGIN"
