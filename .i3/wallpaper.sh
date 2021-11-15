6d04f1265f93be98213e1634947ec30beae3a561
Alex Piechowski <alex@piechowski.io>
2021-11-15 11:32:03 -0600

    Even if feh fails, still sleep for a minute
    This prevents infinite failing loops when feh fails for any reason
    Which eats CPU

--- .i3/wallpaper.sh
+++ .i3/wallpaper.sh
@@ -6,5 +6,5 @@ WALLPAPERS="$HOME"/.i3/wallpaper/
 TIME=1m
 while [ true ];
 do
-    feh --randomize --bg-fill "$WALLPAPERS" && sleep $TIME
+    feh --randomize --bg-fill "$WALLPAPERS"; sleep $TIME
 done &
