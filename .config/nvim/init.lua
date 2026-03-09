872bf857a85d12bc11b3aa74040cee5f95315c45
Alex Piechowski <alex@piechowski.io>
2026-03-09 08:54:56 +0700

    Update nvim plugins
    Switch from nvim-lspconfig to LanguageClient-neovim for LSP support,
    add vim-gitgutter for inline git diff signs, replace vim-crystal with
    jlcrochet/vim-crystal, and move copilot.vim to top of plugin list.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -4,23 +4,25 @@ file:close()
 
 local use = require('packer').use
 require('packer').startup(function()
+  use 'github/copilot.vim'
   use 'wbthomason/packer.nvim'
-  use 'neovim/nvim-lspconfig'
-  require'lspconfig'.crystalline.setup{}
-
+  use {
+    'autozimu/LanguageClient-neovim',
+    branch = 'next',
+    run = 'bash install.sh'
+}
   use 'tpope/vim-commentary'
   use 'tpope/vim-surround'
   use 'tpope/vim-repeat'
+  use 'airblade/vim-gitgutter'
   use 'tpope/vim-fugitive'
   use 'tpope/vim-abolish' -- TODO: learn
 
   use 'christoomey/vim-tmux-navigator'
-  use 'sheerun/vim-polyglot' -- TODO: Is this not working? Had to add vim-crystal below
+  use 'sheerun/vim-polyglot'
+  use 'jlcrochet/vim-crystal'
   use 'w0rp/ale'
   use { 'junegunn/fzf', run = 'cd ~/.fzf && ./install --all' }
   use 'junegunn/fzf.vim'
 
-  use 'vim-crystal/vim-crystal'
-
-  use 'github/copilot.vim'
 end)
